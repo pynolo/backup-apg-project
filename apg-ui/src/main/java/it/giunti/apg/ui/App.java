a4f8fa806d0a9b420e25ef54ded88200cd8f3b8a
Paolo Tacconi <p.tacconi@giunti.it>
2017-06-15 16:34:36 +0200

    Project modular structure

---
+++ apg-ui/src/main/java/it/giunti/apg/ui/App.java
@@ -0,0 +1,13 @@
+package it.giunti.apg.ui;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
