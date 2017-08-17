3d3bd6d19b0c184787b207eeeabe021463d188ac
Paolo Tacconi <p.tacconi@giunti.it>
2017-08-17 10:14:28 +0200

    Reverted to Mojo GWT maven plugin

--- script/apg-mvn-test.sh
+++ script/apg-mvn-test.sh
@@ -6,6 +6,7 @@ mvn install -Ptest
 # Package jobs module
 cd ../apg-automation
 rm -R src/main/webapp/WEB-INF/lib
+mvn clean
 mvn package -Ptest
 mv target/apgautomation.war ~/workspace/
 rm -R src/main/webapp/WEB-INF/lib
@@ -13,6 +14,7 @@ rm -R src/main/webapp/WEB-INF/lib
 # Package web services module
 cd ../apg-ws/
 rm -R src/main/webapp/WEB-INF/lib
+mvn clean
 mvn package -Pbuild
 mv target/apgws.war ~/workspace/
 
@@ -20,8 +22,8 @@ mv target/apgws.war ~/workspace/
 cd ../apg-ui
 rm -R src/main/webapp/apg
 rm -R src/main/webapp/WEB-INF/lib
-mvn gwt:compile -Pbuild
-mvn gwt:package-app -Pbuild
+mvn clean
+mvn package -Pbuild
 mv target/apg.war ~/workspace/
 
 # Leaves in repository apg-core jar with dev profile
