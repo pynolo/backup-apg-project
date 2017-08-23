93dcbf87c72113001cf4da6cfb5edfb36c6524fc
Paolo Tacconi <p.tacconi@giunti.it>
2017-08-23 16:33:17 +0200

    Important clean commands added to mvn scripts

--- script/apg-mvn-os.sh
+++ script/apg-mvn-os.sh
@@ -5,22 +5,30 @@ mvn clean install -Pos
 # Package jobs module
 cd ../apg-automation
 rm -R src/main/webapp/WEB-INF/lib
+rm -R src/main/webapp/WEB-INF/classes
 mvn clean compile war:inplace package -Pos
 mv target/apgautomation.war ~/workspace/
 rm -R src/main/webapp/WEB-INF/lib
+rm -R src/main/webapp/WEB-INF/classes
 
 # Package web services module
 cd ../apg-ws/
 rm -R src/main/webapp/WEB-INF/lib
+rm -R src/main/webapp/WEB-INF/classes
 mvn clean compile war:inplace package -Pbuild
 mv target/apgws.war ~/workspace/
+rm -R src/main/webapp/WEB-INF/lib
+rm -R src/main/webapp/WEB-INF/classes
 
 # Package GWT user interfare
 cd ../apg-ui
 rm -R src/main/webapp/apg
 rm -R src/main/webapp/WEB-INF/lib
+rm -R src/main/webapp/WEB-INF/classes
 mvn clean compile war:inplace package -Pbuild
 mv target/apg.war ~/workspace/
+rm -R src/main/webapp/WEB-INF/lib
+rm -R src/main/webapp/WEB-INF/classes
 
 # Leaves in repository apg-core jar with dev profile
 cd ../apg-core
--- script/apg-mvn-test.sh
+++ script/apg-mvn-test.sh
@@ -5,22 +5,30 @@ mvn clean install -Ptest
 # Package jobs module
 cd ../apg-automation
 rm -R src/main/webapp/WEB-INF/lib
+rm -R src/main/webapp/WEB-INF/classes
 mvn clean compile war:inplace package -Ptest
 mv target/apgautomation.war ~/workspace/
 rm -R src/main/webapp/WEB-INF/lib
+rm -R src/main/webapp/WEB-INF/classes
 
 # Package web services module
 cd ../apg-ws/
 rm -R src/main/webapp/WEB-INF/lib
+rm -R src/main/webapp/WEB-INF/classes
 mvn clean compile war:inplace package -Pbuild
 mv target/apgws.war ~/workspace/
+rm -R src/main/webapp/WEB-INF/lib
+rm -R src/main/webapp/WEB-INF/classes
 
 # Package GWT user interfare
 cd ../apg-ui
 rm -R src/main/webapp/apg
 rm -R src/main/webapp/WEB-INF/lib
+rm -R src/main/webapp/WEB-INF/classes
 mvn clean compile war:inplace package -Pbuild
 mv target/apg.war ~/workspace/
+rm -R src/main/webapp/WEB-INF/lib
+rm -R src/main/webapp/WEB-INF/classes
 
 # Leaves in repository apg-core jar with dev profile
 cd ../apg-core
