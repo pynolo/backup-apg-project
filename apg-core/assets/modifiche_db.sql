a707bdfde3c34596b496f2ee0ff76dbc31544102
Paolo Tacconi <p.tacconi@giunti.it>
2017-10-06 10:29:52 +0200

    Small fixes on queries & css

--- apg-core/assets/modifiche_db.sql
+++ apg-core/assets/modifiche_db.sql
@@ -163,6 +163,6 @@ ALTER TABLE `opzioni_istanze_abbonamenti` CHANGE COLUMN `id_istanza` `id_istanza
 ALTER TABLE anagrafiche ADD COLUMN data_nascita DATE DEFAULT NULL;
 ALTER TABLE anagrafiche ADD COLUMN data_creazione DATETIME DEFAULT NULL;
 update anagrafiche set data_creazione = 
-	(select min(abb.data_creazione) from istanze_abbonamenti as ia 
+	(select min(ia.data_creazione) from istanze_abbonamenti as ia 
 		where anagrafiche.id = ia.id_abbonato or anagrafiche.id = ia.id_pagante);
 update anagrafiche set data_creazione = data_modifica where data_creazione is null;
