7110522fedd5f789e6c36d05ece6aa28994bccd8
Paolo Tacconi <p.tacconi@giunti.it>
2018-05-02 15:31:21 +0200

    Added time shift to getValue in DateOnlyBox

--- apg-ui/src/main/java/it/giunti/apg/client/widgets/DateOnlyBox.java
+++ apg-ui/src/main/java/it/giunti/apg/client/widgets/DateOnlyBox.java
@@ -9,24 +9,35 @@ import com.google.gwt.user.datepicker.client.DatePicker;
 
 public class DateOnlyBox extends DateBox {
 
+	
+	private static int SECURITY_HOUR_OFFSET = 4;
+	
 	public DateOnlyBox() {
 		super();
 	}
 	
 	public DateOnlyBox(DatePicker picker, Date dt, Format format) {
-		super(picker, moveToNoon(dt), format);
+		super(picker, moveToSafeTime(dt), format);
 	}
 	
 	@Override
 	public void setValue(Date date) {
-		super.setValue(moveToNoon(date));
+		super.setValue(moveToSafeTime(date));
 	}
 	
-	private static Date moveToNoon(Date dt) {
+	private static Date moveToSafeTime(Date dt) {
 		if (dt != null) {
 			Long longDt = dt.getTime();
-			longDt += AppConstants.HOUR*12;
+			longDt += AppConstants.HOUR*SECURITY_HOUR_OFFSET;
 			return new Date(longDt);
+		} else return null;
+	}
+
+	@Override
+	public Date getValue() {
+		Date value = super.getValue();
+		if (value != null) {
+			return moveToSafeTime(value);
 		} else return null;
 	}
 	
