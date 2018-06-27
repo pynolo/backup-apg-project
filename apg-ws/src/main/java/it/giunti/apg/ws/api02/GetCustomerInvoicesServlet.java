63c840ba3d327626826ab315fc14003ddc52072a
Paolo Tacconi <p.tacconi@giunti.it>
2018-06-27 10:42:24 +0200

    Implemented hidden bills core changes

--- apg-ws/src/main/java/it/giunti/apg/ws/api02/GetCustomerInvoicesServlet.java
+++ apg-ws/src/main/java/it/giunti/apg/ws/api02/GetCustomerInvoicesServlet.java
@@ -100,7 +100,7 @@ public class GetCustomerInvoicesServlet extends ApiServlet {
 			try {
 				Anagrafiche ana = new AnagraficheDao().findByUid(ses, idCustomer);
 				if (ana == null) throw new BusinessException(idCustomer+" has no match");
-				List<Fatture> fList = new FattureDao().findByAnagrafica(ses, ana.getId(), false);
+				List<Fatture> fList = new FattureDao().findByAnagrafica(ses, ana.getId(), false, true);
 				//nulls id's of prints if prints are not available
 				for (Fatture f:fList) {
 					FattureStampe sf = GenericDao.findById(ses, FattureStampe.class, f.getIdFatturaStampa());
