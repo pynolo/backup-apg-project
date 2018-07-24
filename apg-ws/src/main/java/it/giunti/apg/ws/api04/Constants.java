9d817bb41c7c3e52f0ec4ef39d4a683f2fb9ddc3
Paolo Tacconi <p.tacconi@giunti.it>
2018-07-24 11:46:05 +0200

    Added API 4.0

---
+++ apg-ws/src/main/java/it/giunti/apg/ws/api04/Constants.java
@@ -0,0 +1,106 @@
+package it.giunti.apg.ws.api04;
+
+import java.text.SimpleDateFormat;
+
+public class Constants {
+
+	public static final SimpleDateFormat FORMAT_API_DATE = new SimpleDateFormat("yyyy-MM-dd");
+
+	public static final String USER_API = "api";
+	
+	public static final String PATTERN_FIND_ISSUES = "/find_issues";
+	public static final String PATTERN_GET_SUBSCRIPTION_ISSUES = "/get_subscription_issues";
+	public static final String PATTERN_GET_CUSTOMER = "/get_customer";
+	public static final String PATTERN_CREATE_CUSTOMER = "/create_customer";
+	public static final String PATTERN_UPDATE_CUSTOMER = "/update_customer";
+	public static final String PATTERN_AUTHENTICATE_BY_COD_ABBO = "/authenticate_by_cod_abbo";
+	public static final String PATTERN_REQUEST_CUSTOMER_MERGE = "/request_customer_merge";
+	public static final String PATTERN_GET_CUSTOMER_SUBSCRIPTIONS = "/get_customer_subscriptions";
+	public static final String PATTERN_GET_SUBSCRIPTION = "/get_subscription";
+	public static final String PATTERN_CREATE_SUBSCRIPTION = "/create_subscription";
+	public static final String PATTERN_UPDATE_OFFERING = "/update_offering";
+	public static final String PATTERN_UPDATE_SUBSCRIPTION_OFFERING = "/update_subscription_offering";
+	public static final String PATTERN_UPDATE_OPTIONS = "/update_options";
+	public static final String PATTERN_PAY_SUBSCRIPTION = "/pay_subscription";
+	public static final String PATTERN_GET_CUSTOMER_INVOICES = "/get_customer_invoices";
+	public static final String PATTERN_GET_INVOICE = "/get_invoice";
+	public static final String PATTERN_GET_PAYMENT_FORM = "/get_payment_form";
+	public static final String PATTERN_GET_INVOICE_FILE = "/get_invoice_file";
+	public static final String PATTERN_GET_PAYMENT_FORM_FILE = "/get_payment_form_file";
+	public static final String PATTERN_GET_OFFERING = "/get_offering";
+	public static final String PATTERN_GET_OPTION = "/get_option";
+	public static final String PATTERN_FIND_SUBSCRIPTIONS_BY_ACTION = "/find_subscriptions_by_action";
+	public static final String PATTERN_FIND_MODIFIED_CRM_DATA = "/find_modified_crm_data";
+	public static final String PATTERN_FIND_DELETED_CRM_DATA = "/find_deleted_crm_data";
+	
+	public static final String PARAM_ACCESS_KEY = "access_key";
+	public static final String PARAM_ID_MAGAZINE = "id_magazine";
+	public static final String PARAM_DT_BEGIN = "dt_begin";
+	public static final String PARAM_DT_END = "dt_end";
+	public static final String PARAM_ID_SUBSCRIPTION = "id_subscription";
+	public static final String PARAM_ID_CUSTOMER = "id_customer";
+	public static final String PARAM_ID_CUSTOMER_PROPOSED = "id_customer_proposed";
+	public static final String PARAM_ID_INVOICE = "id_invoice";
+	public static final String PARAM_HUMAN_CHECK = "human_check";
+	public static final String PARAM_SEX = "sex";
+	public static final String PARAM_ADDRESS_FIRST_NAME = "address_first_name"; 
+	public static final String PARAM_ADDRESS_LAST_NAME_COMPANY = "address_last_name_company";
+	public static final String PARAM_ADDRESS_TITLE = "address_title";
+	public static final String PARAM_ADDRESS_CO = "address_co";
+	public static final String PARAM_ADDRESS_ADDRESS = "address_address"; 
+	public static final String PARAM_ADDRESS_LOCALITY = "address_locality"; 
+	public static final String PARAM_ADDRESS_PROVINCE = "address_province"; 
+	public static final String PARAM_ADDRESS_ZIP = "address_zip"; 
+	public static final String PARAM_ADDRESS_COUNTRY_CODE = "address_country_code";
+	public static final String PARAM_BILLING_FIRST_NAME = "billing_first_name"; 
+	public static final String PARAM_BILLING_LAST_NAME_COMPANY = "billing_last_name_company";
+	public static final String PARAM_BILLING_TITLE = "billing_title";
+	public static final String PARAM_BILLING_CO = "billing_co";
+	public static final String PARAM_BILLING_ADDRESS = "billing_address";
+	public static final String PARAM_BILLING_LOCALITY = "billing_locality";
+	public static final String PARAM_BILLING_PROVINCE = "billing_province";
+	public static final String PARAM_BILLING_ZIP = "billing_zip";
+	public static final String PARAM_BILLING_COUNTRY_CODE = "billing_country_code";
+	public static final String PARAM_COD_FISC = "cod_fisc";
+	public static final String PARAM_PIVA = "piva";
+	public static final String PARAM_PHONE_MOBILE = "phone_mobile";
+	public static final String PARAM_PHONE_LANDLINE = "phone_landline";
+	public static final String PARAM_EMAIL_PRIMARY = "email_primary";
+	public static final String PARAM_EMAIL_SECONDARY = "email_secondary";
+	public static final String PARAM_ID_JOB = "id_job";
+	public static final String PARAM_ID_QUALIFICATION = "id_qualification";
+	public static final String PARAM_ID_TIPO_ANAGRAFICA = "id_tipo_anagrafica";
+	public static final String PARAM_CONSENT_TOS = "consent_tos";
+	public static final String PARAM_CONSENT_MARKETING = "consent_marketing";
+	public static final String PARAM_CONSENT_PROFILING = "consent_profiling";
+	public static final String PARAM_CONSENT_UPDATE_DATE = "consent_update_date";
+	public static final String PARAM_COD_ABBO = "cod_abbo";
+	public static final String PARAM_ID_OFFERING = "id_offering";
+	public static final String PARAM_ID_RENEWAL_OFFERING = "id_renewal_offering";
+	public static final String PARAM_ID_OPTION = "id_option";
+	public static final String PARAM_ID_CUSTOMER_RECIPIENT = "id_customer_recipient";
+	public static final String PARAM_ID_CUSTOMER_PAYER = "id_customer_payer";
+	public static final String PARAM_ID_CUSTOMER_PROMOTER = "id_customer_promoter";
+	public static final String PARAM_OPTIONS = "options";
+	public static final String PARAM_QUANTITY = "quantity";
+	public static final String PARAM_CM_FIRST_ISSUE = "cm_first_issue";
+	public static final String PARAM_CM_LAST_ISSUE = "cm_last_issue";
+	public static final String PARAM_ID_PAYMENT_TYPE = "id_payment_type";
+	public static final String PARAM_PAYMENT_AMOUNT = "payment_amount";
+	public static final String PARAM_PAYMENT_DATE = "payment_date";
+	public static final String PARAM_PAYMENT_TRN = "payment_trn";
+	public static final String PARAM_PAYMENT_NOTE = "payment_note";
+	public static final String PARAM_BIRTH_DATE = "birth_date";
+	public static final String PARAM_RENEWAL_DATE = "renewal_date";
+	public static final String PARAM_ACTION = "action";
+	public static final String PARAM_PAGE = "page";
+	public static final String PARAM_CUSTOMER_TYPE = "customer_type";
+	public static final String PARAM_CREATION_DATE = "creation_date";
+	public static final String PARAM_MODIFIED_DATE = "modified_date";
+	
+	public static final String VALUE_ACTION_CHARGE_WARNING = "CHARGE_WARNING";
+	public static final String VALUE_ACTION_CHARGE = "CHARGE";
+	public static final String VALUE_ACTION_RENEWAL_WARNING = "RENEWAL_WARNING";
+	public static final String VALUE_ACTION_RENEWAL = "RENEWAL";
+	
+}
---
+++ apg-ws/src/main/java/it/giunti/apg/ws/api04/GetCustomerSubscriptionsServlet.java
@@ -0,0 +1,183 @@
+package it.giunti.apg.ws.api04;
+
+import it.giunti.apg.core.persistence.AnagraficheDao;
+import it.giunti.apg.core.persistence.IstanzeAbbonamentiDao;
+import it.giunti.apg.core.persistence.SessionFactory;
+import it.giunti.apg.shared.AppConstants;
+import it.giunti.apg.shared.BusinessException;
+import it.giunti.apg.shared.model.Anagrafiche;
+import it.giunti.apg.shared.model.ApiServices;
+import it.giunti.apg.shared.model.IstanzeAbbonamenti;
+import it.giunti.apg.ws.business.ValidationBusiness;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonBuilderFactory;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/*@WebServlet(Constants.PATTERN_API04+Constants.PATTERN_GET_CUSTOMER_SUBSCRIPTIONS)*/
+public class GetCustomerSubscriptionsServlet extends ApiServlet {
+	private static final long serialVersionUID = -8580433062665293806L;
+	private static final String FUNCTION_NAME = Constants.PATTERN_GET_CUSTOMER_SUBSCRIPTIONS;
+	private static final Logger LOG = LoggerFactory.getLogger(GetCustomerSubscriptionsServlet.class);
+
+	/*example testing url:
+	 http://127.0.0.1:8888/api01/get_customer_subscriptions?access_key=1234&id_customer=Q090NQ
+	*/
+
+    public GetCustomerSubscriptionsServlet() {
+        super();
+        LOG.info(FUNCTION_NAME+" started");
+    }
+
+	@Override
+	public void init() throws ServletException {
+		super.init();
+	}
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+	}
+
+	//@Override
+	//protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	//	doPost(request, response);
+	//}
+    
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+    @Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		BaseUrlSingleton.get().setBaseUrl(request);
+		request.setCharacterEncoding(AppConstants.CHARSET_UTF8);
+		JsonObject result = null;
+		//acquire access key
+		String accessKey = request.getParameter(Constants.PARAM_ACCESS_KEY);
+		ApiServices service = null;
+		if (accessKey == null) {
+			result = BaseJsonFactory.buildBaseObject(ErrorEnum.WRONG_ACCESS_KEY, ErrorEnum.WRONG_ACCESS_KEY.getErrorDescr());
+		} else {
+			try {
+				service = ValidationBusiness.validateAccessKey(accessKey);
+			} catch (BusinessException e) {
+				result = BaseJsonFactory.buildBaseObject(ErrorEnum.INTERNAL_ERROR, ErrorEnum.INTERNAL_ERROR.getErrorDescr());
+				LOG.error(e.getMessage(), e);
+			}
+			if (service == null) {
+				result = BaseJsonFactory.buildBaseObject(ErrorEnum.WRONG_ACCESS_KEY, ErrorEnum.WRONG_ACCESS_KEY.getErrorDescr());
+			} else {
+				LOG.debug(FUNCTION_NAME+" chiamata da "+service.getNome());
+			}
+		}
+		//acquire idSubscription
+		String idCustomer = request.getParameter(Constants.PARAM_ID_CUSTOMER);
+		if (idCustomer == null) {
+			result = BaseJsonFactory.buildBaseObject(ErrorEnum.EMPTY_PARAMETER, Constants.PARAM_ID_CUSTOMER+" is empty");
+		}
+		//build response
+		if (result == null) {
+			Session ses = SessionFactory.getSession();
+			try {
+				Anagrafiche ana = new AnagraficheDao().findByUid(ses, idCustomer);
+				if (ana == null) throw new BusinessException(idCustomer+" has no match");
+				List<IstanzeAbbonamenti> iaProprieList = new IstanzeAbbonamentiDao().findIstanzeProprieByAnagrafica(ses, ana.getId(), false, 0, Integer.MAX_VALUE);
+				List<IstanzeAbbonamenti> iaRegalateList = new IstanzeAbbonamentiDao().findIstanzeRegalateByAnagrafica(ses, ana.getId(), false, 0, Integer.MAX_VALUE);
+				List<IstanzeAbbonamenti> iaList = new ArrayList<IstanzeAbbonamenti>();
+				//iaList.addAll(filterIstanze(iaProprieList));
+				//iaList.addAll(filterIstanze(iaRegalateList));
+				iaList.addAll(iaProprieList);
+				iaList.addAll(iaRegalateList);
+				JsonObjectBuilder joBuilder = schemaBuilder(iaList);
+				result = BaseJsonFactory.buildBaseObject(joBuilder);
+			} catch (BusinessException e) {
+				result = BaseJsonFactory.buildBaseObject(ErrorEnum.DATA_NOT_FOUND, ErrorEnum.DATA_NOT_FOUND.getErrorDescr());
+				LOG.info(e.getMessage(), e);
+			} finally {
+				ses.close();
+			}
+		}
+		//send response
+		response.setContentType("application/json");
+		response.setCharacterEncoding(AppConstants.CHARSET_UTF8);
+		PrintWriter out = response.getWriter();
+		out.print(result.toString());
+		out.flush();
+	}
+
+	private JsonObjectBuilder schemaBuilder(List<IstanzeAbbonamenti> iaList) throws BusinessException {
+		JsonBuilderFactory factory = Json.createBuilderFactory(null);
+		JsonArrayBuilder arrayBuilder = factory.createArrayBuilder();
+		Session ses = SessionFactory.getSession();
+		try {
+			for (IstanzeAbbonamenti ia:iaList) {
+				JsonObjectBuilder ob = GetSubscriptionServlet.subscriptionSchemaBuilder(ses, ia);
+				//JsonObjectBuilder ob = factory.createObjectBuilder();
+				//add(ob, Constants.PARAM_COD_ABBO, ia.getAbbonamento().getCodiceAbbonamento());
+				//add(ob, Constants.PARAM_ID_SUBSCRIPTION, ia.getId());
+				//add(ob, Constants.PARAM_ID_MAGAZINE, ia.getAbbonamento().getPeriodico().getUid());
+				//add(ob, Constants.PARAM_ID_OFFERING, ia.getListino().getUid());
+				//add(ob, Constants.PARAM_ID_CUSTOMER_RECIPIENT, ia.getAbbonato().getUid());
+				//if (ia.getPagante() != null) add(ob, Constants.PARAM_ID_CUSTOMER_PAYER, ia.getPagante().getUid());
+				//add(ob, "subscription_begin", ia.getFascicoloInizio().getDataInizio());
+				//add(ob, "subscription_end", ia.getFascicoloFine().getDataFine());
+				arrayBuilder.add(ob);
+			}
+		} catch (HibernateException e) {
+			throw new BusinessException(e.getMessage(), e);
+		} finally {
+			ses.close();
+		}
+		JsonObjectBuilder objectBuilder = factory.createObjectBuilder();
+		objectBuilder.add("subscriptions", arrayBuilder);
+		return objectBuilder;
+	}
+
+//	private List<IstanzeAbbonamenti> filterIstanze(List<IstanzeAbbonamenti> iaList) {
+//		List<IstanzeAbbonamenti> resultList = new ArrayList<IstanzeAbbonamenti>();
+//		Map<String, IstanzeAbbonamenti> istanzeMap = new HashMap<String, IstanzeAbbonamenti>();
+//		Date now = DateUtil.now();
+//		for (IstanzeAbbonamenti ia:iaList) {
+//			if (!ia.getInvioBloccato()) {
+//				if (ia.getFascicoloInizio().getDataInizio().after(now)) {
+//					//Abbonamento futuro
+//					resultList.add(ia);
+//				} else {
+//					//Abbonamento corrente o concluso
+//					String lettera = ia.getAbbonamento().getPeriodico().getLettera();
+//					IstanzeAbbonamenti mapIa = istanzeMap.get(lettera);
+//					if (mapIa == null) {
+//						istanzeMap.put(lettera, ia);
+//					} else {
+//						if (mapIa.getFascicoloInizio().getDataInizio().before(ia.getFascicoloInizio().getDataInizio())) {
+//							//mapIa è precedente ad ia
+//							istanzeMap.put(lettera, ia);
+//						} //else: non sostituisce mapIa nella mappa
+//					}
+//				}
+//			}
+//		}
+//		//Copia la mappa nell'array finale
+//		for (String l:istanzeMap.keySet()) {
+//			resultList.add(istanzeMap.get(l));
+//		}
+//		return resultList;
+//	}
+}
