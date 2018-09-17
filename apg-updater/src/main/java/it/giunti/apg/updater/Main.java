b52fcb651127485508ea26bc6c96f7e9866e6eb5
Paolo Tacconi <p.tacconi@giunti.it>
2018-09-17 10:40:35 +0200

    Small fix (oops)

--- apg-updater/src/main/java/it/giunti/apg/updater/Main.java
+++ apg-updater/src/main/java/it/giunti/apg/updater/Main.java
@@ -8,7 +8,7 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		try {
-			OutputWrongCodFisc.run(false);
+			OutputWrongCodFisc.run();
 			//CinVerification.update();
 			//EncryptGdprPasswords.execute();
 			//InsertAnagraficaAndArticolo.parseFileAnagrafiche(args[0], args[1]);// args: "file" "lettera"
