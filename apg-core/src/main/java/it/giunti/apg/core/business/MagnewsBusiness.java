0bae443d9dbf6c7a94e2486d154d2bbac0950ce6
Paolo Tacconi <p.tacconi@giunti.it>
2018-12-10 09:10:32 +0100

    Temporary suspended development

--- apg-core/src/main/java/it/giunti/apg/core/business/MagnewsBusiness.java
+++ apg-core/src/main/java/it/giunti/apg/core/business/MagnewsBusiness.java
@@ -2,6 +2,7 @@ package it.giunti.apg.core.business;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
@@ -13,9 +14,12 @@ import it.giunti.apg.core.persistence.ConfigDao;
 import it.giunti.apg.core.persistence.SessionFactory;
 import it.giunti.apg.shared.AppConstants;
 import it.giunti.apg.shared.BusinessException;
+import it.giunti.apg.soap.magnews.BatchMergeValues;
 import it.giunti.apg.soap.magnews.Credentials;
 import it.giunti.apg.soap.magnews.MagNewsAPI;
 import it.giunti.apg.soap.magnews.MagNewsAPIService;
+import it.giunti.apg.soap.magnews.OnDemandMessageOptions;
+import it.giunti.apg.soap.magnews.Option;
 
 public class MagnewsBusiness {
 	private static final Logger LOG = LoggerFactory.getLogger(MagnewsBusiness.class);
@@ -40,7 +44,10 @@ public class MagnewsBusiness {
 		throw new BusinessException(AppConstants.MSG_EMPTY_RESULT);
 	}
 	
-	public static void connect() throws BusinessException {
+	public static void batchMergeContactAndSendOnDemandMessage(String idNewsletter,
+	        String idDatabase, List<BatchMergeValues> values,
+	        List<Option> mergeOptions, List<OnDemandMessageOptions> options) 
+	        			throws BusinessException {
 		URL serviceUrl = null;
 		try {
 			serviceUrl = new URL(ServerConstants.MAGNEWS_WSDL);
@@ -48,11 +55,13 @@ public class MagnewsBusiness {
 			throw new BusinessException(e.getMessage(), e);
 		}
 		String password = getAccessToken();
-		Credentials c = new Credentials();
-		c.setPassword(password);
+		Credentials credentials = new Credentials();
+		credentials.setPassword(password);
 
 		MagNewsAPIService service = new MagNewsAPIService(serviceUrl);
 		MagNewsAPI port = service.getMagNewsAPIPort();
+		
+		port.batchMergeContactAndSendOnDemandMessage(idNewsletter, idDatabase, values, mergeOptions, options, credentials);
 		
 	}
 }
