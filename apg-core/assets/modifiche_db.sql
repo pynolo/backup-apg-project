5fbfcd3e12411b2bea40e8076ce4cadce6dd060e
Paolo Tacconi <p.tacconi@giunti.it>
2019-06-20 16:43:45 +0200

    POP update query

--- apg-core/assets/modifiche_db.sql
+++ apg-core/assets/modifiche_db.sql
@@ -1,2 +1,5 @@
-ALTER TABLE istanze_abbonamenti ADD COLUMN proposta_acquisto bit(1) NOT NULL default false;
-
+#Update istanze esistenti
+ALTER TABLE `istanze_abbonamenti` ADD COLUMN `proposta_acquisto` bit(1) NOT NULL DEFAULT false;
+update istanze_abbonamenti set proposta_acquisto=true where 
+	id_listino in (select lst.id from listini lst inner join tipi_abbonamento ta on (ta.id=lst.id_tipo_abbonamento) where
+		ta.codice like 'POP');
