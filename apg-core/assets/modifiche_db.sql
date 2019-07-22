400e52ab7a4560109ef2815741e50582e032f87c
Paolo Tacconi <p.tacconi@giunti.it>
2019-07-22 10:46:24 +0200

    version = 6.8.14 + Added new cap

--- apg-core/assets/modifiche_db.sql
+++ apg-core/assets/modifiche_db.sql
@@ -3,3 +3,8 @@ ALTER TABLE `istanze_abbonamenti` ADD COLUMN `proposta_acquisto` bit(1) NOT NULL
 update istanze_abbonamenti set proposta_acquisto=true where 
 	id_listino in (select lst.id from listini lst inner join tipi_abbonamento ta on (ta.id=lst.id_tipo_abbonamento) where
 		ta.codice like 'POP');
+
+###
+
+insert into localita (cap,nome,id_provincia, modifica_propagata) values ('22023','Centro Valle Intelvi','CO', false);
+
