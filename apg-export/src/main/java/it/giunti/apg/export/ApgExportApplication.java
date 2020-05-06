7fd45eeed5e82c11ba50dc241ba06a786f3bd436
Paolo Tacconi <p.tacconi@giunti.it>
2020-05-06 08:57:25 +0200

    Added default spring boot scheduler

--- apg-export/src/main/java/it/giunti/apg/export/ApgExportApplication.java
+++ apg-export/src/main/java/it/giunti/apg/export/ApgExportApplication.java
@@ -1,19 +1,19 @@
 package it.giunti.apg.export;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import it.giunti.apg.export.service.ExportService;
 
 @SpringBootApplication
+@EnableScheduling
 public class ApgExportApplication {
 
-	private static Logger LOG = LoggerFactory.getLogger(ApgExportApplication.class);
+	//private static Logger LOG = LoggerFactory.getLogger(ApgExportApplication.class);
 
 	public static final String LAST_EXPORT_TIMESTAMP="last_export";
 	public static final Integer PAGING=250;
@@ -25,8 +25,8 @@ public class ApgExportApplication {
 		SpringApplication.run(ApgExportApplication.class, args);
 	}
 
-    @EventListener(ApplicationReadyEvent.class)
-    public void runAfterStartup() {
-    	exportService.runExport();
-    }
+	@EventListener(ApplicationReadyEvent.class)
+	public void runAfterStartup() {
+		exportService.runExport();
+	}
 }
---
+++ apg-export/src/main/java/it/giunti/apg/export/scheduler/ScheduledTasks.java
@@ -0,0 +1,26 @@
+package it.giunti.apg.export.scheduler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import it.giunti.apg.export.service.ExportService;
+
+
+@Component
+public class ScheduledTasks {
+
+	private static final Logger LOG = LoggerFactory.getLogger(ScheduledTasks.class);
+
+	@Autowired
+	ExportService exportService;
+
+	@Scheduled(cron = "0 0 23 * * MON-FRI")
+	public void reportCurrentTime() {
+		LOG.debug("Started task: crm export");
+		exportService.runExport();
+		LOG.debug("Finished task: crm export");
+	}
+}
